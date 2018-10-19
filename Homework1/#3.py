dd19986ea68c31d8128867f1bfc691b19ebca546
pzabauski <pzabauski@icloud.comD>
2018-10-19 13:15:22 +0300

    2nd commit

--- Homework1/#3.py
+++ Homework1/#3.py
@@ -1,22 +1,22 @@
 # Пользователь вводит целое число n. При помощи решета Эрастофена найти все простые числа меньше n
 
-n = 6 #input("Введите целое число")
-n = int(n)
-
+n = int(input("Введите число"))
 seq = []
 
-for i in range(1, 5+1):
+for i in range(1, n + 1):
     seq.append(i)
+
 print(seq)
 
 
-j = 0
-while j < 5:
-    x = seq[j]
-    print(x)
-    j += 1
-    for a in range(1, 5):
-        if a % x == 0:
+for i in range(1, len(seq)):
+    x = seq[i]
+    if x == 0:
+        continue
+    for j in range(i+1, len(seq)):
+        if seq[j] % x == 0:
             seq[j] = 0
+
 print(seq)
+
 
